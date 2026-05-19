a23d42ed9403ecfaf6275740cae8afe857a758c7
Morris Smith <MorrisTuwaSmith@gmail.com>
2026-05-19 19:39:40 -0400

    update backend.tf workspace name

--- TF/backend.tf
+++ TF/backend.tf
@@ -7,7 +7,7 @@
 
         # The name of the Terraform Cloud workspace to store Terraform state files in.
         workspaces {
-          name = "library-analytics-tf"
+          name = "library-analytics-dev"
         }
       }
     }
