8ea1aad7715872d0124811c8b59e86e477e85fed
Morris Smith <MorrisTuwaSmith@gmail.com>
2026-05-25 19:24:08 -0400

    add terraform wirkspace to provder.tf

--- TF/provider.tf
+++ TF/provider.tf
@@ -1,7 +1,7 @@
 # provider.tf - Configure AWS provider and data sources
 
 terraform {
-  required_version = ">= 1.0"
+  required_version = "~> 1.15.0"
 
   required_providers {
     aws = {
@@ -11,7 +11,12 @@ terraform {
   }
 
   cloud {
+    hostname     = "app.terraform.io"
     organization = "TuwaAnalytics"
+
+    workspaces {
+      name = "library-analytics-dev"
+    }
   }
 }
 
