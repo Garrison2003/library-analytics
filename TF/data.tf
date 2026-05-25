9275f9103bd2076c7a618df6336df3c2b96f0482
Garrison2003 <smithgarrison188@gmail.com>
2026-05-25 01:03:18 -0400

    change runtime from node to python

--- TF/data.tf
+++ TF/data.tf
@@ -1,5 +1,6 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "../lambdas/timeSeriesLambda/dist"
+  source_dir  = "../lambdas/timeSeriesLambda"
   output_path = "lambda_function_payload.zip"
+  excludes    = [".git", "__pycache__", "*.pyc", ".gitignore", "venv", ".venv"]
 }
